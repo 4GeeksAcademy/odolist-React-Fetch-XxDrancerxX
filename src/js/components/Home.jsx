56fb3bf11149192797c5cdedfc3659e32180e80e
XxDrancerxX <idmdiazmora98@gmail.com>
2025-03-14 23:19:05 +0000

    first fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [input, setInput] = useState("");
+	const [list, setList] = useState(["make the homework", "Do excercises", "Do the laundry"]);
+	const [posts, setPosts] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const deleteLi = (index) => {
+		const newtList = list.filter((item, i) => i != index);
+		setList(newtList);
+	}
+      
+	 const toDoUrl = "https://playground.4geeks.com/todo/users/idm";
+	 const getToDos = () => {
+	 	 fetch(toDoUrl)
+		.then((rsp)=> rsp.json())
+		.then((data)=> console.log(data,"data processed"))
+	 };
+         getToDos();    
+	
+
+	return (
+
+		<div className="container d-flex justify-content-center hv-100 ">
+					<ul className="list-group">
+				<li className="list-group-item active" aria-current="true">
+					<input
+						type="text"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								setList([...list, input]);
+								setInput("");
+
+
+							}
+						}}
+					/>
+				</li>
+				{list.map(
+					(item, index) => {
+						return (
+							<li onClick={() => deleteLi(index)} className="list-group-item">{item}</li>
+						)
+					}
+
+				)}
+
+
+			</ul>
 		</div>
 	);
+
 };
 
 export default Home;
