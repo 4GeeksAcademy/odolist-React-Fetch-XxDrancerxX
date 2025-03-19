71d91d0d6e0e25c6af8dff6da2fd7e988f53188b
XxDrancerxX <idmdiazmora98@gmail.com>
2025-03-19 14:42:40 +0000

    a

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,86 +2,89 @@ import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { use } from "react";
 
 //create your first component
 const Home = () => {
-	const [input, setInput] = useState("");
-	const [list, setList] = useState([""]);
-	
+    const [input, setInput] = useState("");
+    const [list, setList] = useState([""]);
 
-	const deleteLi = (index) => {
-		const newtList = list.filter((item, i) => i != index);
-		setList(newtList);
-	}
-      
-	 const toDoUrl = "https://playground.4geeks.com/todo/";
-	//  const toDoUrl = "https://playground.4geeks.com/todo/users/idm";
-	 const getToDos = () => {
-	 	 fetch(toDoUrl + "users/idm")
-		.then((rsp)=> rsp.json())
-		.then((data)=> setList(data.todos))
-	}
-	const addTodo = (input) => {
-		const options = {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			  },
-			  body: JSON.stringify(input)
-			
-		}
-            .fetch(toDoUrl + "todos/idm", options)
-			.then(rsp => rsp.json()) 
-			.then(data => console.log(data,"data added"));
-			
-	}
-	
+    const toDoUrl = "https://playground.4geeks.com/todo/";
 
-	useEffect(()=>{
-     getToDos();        
-	},[]);
-	
-
-	return (
-
-		<div className="container d-flex justify-content-center hv-100 ">
-					<ul className="list-group">
-				<li className="list-group-item active" aria-current="true">
-					<input
-						type="text"
-						value={input}
-						onChange={(e) => setInput(e.target.value)}
-						onKeyDown={(e) => {
-							if (e.key === "Enter") {
-								const todoObj = {
-									"label": input,
-									"is_done": false
-								  }
-								
-								setList([...list, todoObj, ]);
-								setInput("");
-								addTodo(todoObj);
+    const deleteLi = (index) => {
+        const newList = list.filter((item, i) => i !== index);
+        setList(newList);
+        // updateToDos(newList);
+    }
 
 
-							}
-						}}
-					/>
-				</li>
-				{list.length > 0 ? list.map(
-					(item, index) => {
-						return (
-							<li onClick={() => deleteLi(index)} className="list-group-item">{item.label}</li>
-						)
-					}
 
-				)
- : "There is no items"}
+    const getToDos = () => {
+        fetch(toDoUrl + "users/idm")
+            .then((resp) => resp.json())
+            .then((data) => setList(data.todos))
 
-			</ul>
-		</div>
-	);
+    }
+    const addTodo = (input) => {
+        const options = {
+            method: "POST",
+            body: JSON.stringify(input),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+        fetch(toDoUrl + "users/idm", options)
+            .then((resp) => resp.json())
+            .then((data) => data)
 
+
+    }
+
+    useEffect(() => {
+
+        getToDos()
+
+    }
+        , [])
+
+
+
+    return (
+        <div className="container d-flex justify-content-center hv-100 ">
+            <ul className="list-group">
+                <li className="list-group-item active" aria-current="true">
+                    <input
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={(e) => {
+                            console.log("Key pressed:", e.key);
+                            const todoObj = {
+                                "label": input,
+                                "is_done": true
+                            }
+                            console.log("Adding todo:", todoObj);
+                             addTodo(todoObj) 
+                            if (e.key === "Enter") {
+                                setList([...list, todoObj]);
+                               
+                                setInput("");
+                            }
+                        }}
+                    />
+                </li>
+                {list.length > 0 ? list.map(
+                    (item, index) => {
+                        return (
+                            <li key={index} onClick={() => deleteLi(index)} className="list-group-item">{item.label}</li>
+
+                        )
+                    }
+                ) : "There is no Todos"}
+            </ul>
+            <button className="btn btn-danger mt-3">Clean All Tasks</button>
+        </div>
+    );
 };
 
 export default Home;
+
